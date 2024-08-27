16b42a42b3fa21d20030ef686604e2e0fa267632
fridayfairy <fridayfairy@sina.com>
2024-08-27 11:04:08 +0800

    Fix: miss files

--- finetune/merge_lora.py
+++ finetune/merge_lora.py
@@ -1,12 +1,16 @@
 """
-
-https://modelbest.feishu.cn/wiki/MPkPwvONEiZm3BkWMnyc83Tin4d
+# Author： YCX
+# Date:    2024.08.27
+# Description:  把生成的lora文件合并到源模型中
 """
+
 from peft import PeftModel
 from transformers import AutoModel,AutoTokenizer
-model_type="/data1/home/ycx/workspace/HF/MiniCPM-V-2_6" # local_model_path or openbmb/MiniCPM-V-2.5
+import shutil
+import os
+model_type="/data1/home/ycx/workspace/HF/MiniCPM-V-2_6" # 官方下载的原始模型
 path_to_adapter="/data1/home/ycx/ycxGit/MiniCPM-V26/finetune/output/output__lora/checkpoint-10000" # lora保存的地址
-merge_path="./merged_cpm26" # 希望将lora合并到主模型后的保存地址
+merge_path="./MiniCPM-V-2_6-OD" # 希望将lora合并到主模型后的保存地址
 
 model =  AutoModel.from_pretrained(
         model_type,
@@ -29,4 +33,22 @@ print(f"save {merge_path}")
 # 加载分词文件与模型保存到merge后的模型地址
 tokenizer=AutoTokenizer.from_pretrained(model_type,trust_remote_code=True)
 tokenizer.save_pretrained(merge_path)
+
+# 将缺少的文件复制到新模型路径下
+copy_files = ['image_processing_minicpmv.py', 'preprocessor_config.json', 'processing_minicpmv.py']
+
+for file_name in copy_files:
+    src_path = os.path.join(model_type, file_name)
+    dst_path = os.path.join(merge_path, file_name)
+
+    # 复制文件
+    try:
+        shutil.copy(src_path, dst_path)
+        print(f"File '{src_path}' has been copied to '{dst_path}'")
+    except shutil.Error as e:
+        # 处理文件复制中的错误，例如文件不存在或权限问题
+        print(f"Error: {e}")
+    except IOError as e:
+        # 处理文件操作中的I/O错误
+        print(f"I/O Error: {e}")
 
